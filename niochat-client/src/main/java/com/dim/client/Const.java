f05cae67018596fab4b1611e8b3bdbcc2169df58
shuaizhuaidym <shuaizhuaidym@sina.com>
2015-03-12 18:11:53 +0800

    write server address and port to const file

---
+++ niochat-client/src/main/java/com/dim/client/Const.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dim.client;
+
+/**
+ *
+ * @author yanming_dai
+ */
+public class Const {
+    public static final String server="192.168.9.128";
+    public static final Integer port=9600;
+    
+}
